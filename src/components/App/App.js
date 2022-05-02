b410f3e9d94120948d63b3e7a5908c3c7fd3b7c8
AlejandroPinto99 <00156618@uca.edu.sv>
2022-05-01 18:17:44 -0600

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Typography variant="p" component="p" style={{fontFamily: 'Lato'}}>Expediente Clinico Electronico</Typography>
+    </div>
+  );
+}
+
+export default App;
